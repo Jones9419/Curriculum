4226d06d08af59e659d2ca436ef894ba8459ba72
Brent Jones <jones9419@MacBook-Pro.local>
2023-02-25 00:58:19 -0500

    update

---
+++ JS Vacation/index.js
@@ -0,0 +1,14 @@
+const numberOfPeople = 6;
+
+const location = "Florida";
+
+let weather = "Sunny";
+
+const cost = 4042.37;
+
+var list = [
+    {Hotdogs},
+    {Salmon},
+    {Hamburger},
+    {Nachos}
+];
